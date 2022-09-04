7893ef38fd67637d12e4113f8a3a0540c149916a
Muhammad Akmal Hakim <akmalhakim2106@gmail.com>
2022-09-04 12:48:08 +0700

    changing project name

--- akmalee/asgi.py
+++ akmalee/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for exercise0 project.
+ASGI config for akmalee project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exercise0.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'akmalee.settings')
 
 application = get_asgi_application()
--- akmalee/wsgi.py
+++ akmalee/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for exercise0 project.
+WSGI config for akmalee project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exercise0.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'akmalee.settings')
 
 application = get_wsgi_application()
